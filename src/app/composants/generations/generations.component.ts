74d90382302f5ca29ed884b456541c16103fa744
mariebouvard <mariebouvard@outlook.com>
2022-03-23 16:19:21 +0100

    Initialization of the Pokemon application in Angular - add of generation menu

---
+++ src/app/composants/generations/generations.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generations',
+  templateUrl: './generations.component.html',
+  styleUrls: ['./generations.component.css']
+})
+export class GenerationsComponent implements OnInit {
+
+  public generations = ["Génération I", "Génération II", "Génération III", "Génération IV", "Génération V", "Génération VI", "Génération VII"];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
